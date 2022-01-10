9da8eb9b3fc91e895f6c8771aa0c47ee3c9d9f4b
agnese.gurecka <agnese.gurecka@testdevlab.com>
2022-01-10 18:06:22 +0200

    dmkdv

--- lib/core/device.rb
+++ lib/core/device.rb
@@ -742,7 +742,7 @@ class Device
     @driver.set_context(context)
   end
 
-  def grep_string(action = nil)
+  def grep_string(action)
 
     string = convert_value(action["Value"]) # incoming string
     greps = action["Greps"]
